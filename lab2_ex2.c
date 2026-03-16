a0c6ac9e732092e95955cba35a09e41fa997e4b8
jak-alex <alex.jakimik@gmail.com>
2026-03-16 18:25:15 +0200

    Refactor calculations in lab2_ex2.c
    Updated variable types and removed zero division check.

--- lab2_ex2.c
+++ lab2_ex2.c
@@ -3,28 +3,22 @@
 
 int main() {
     // Оголошення сталих змінних
-    double a = 2.0;
-    double b = -2.0;
-    double c = 22.0;
+    const int a = 2;
+    const int b = -2;
+    const int c = 22;
     double y;
 
     printf("Обчислення значення функції:\n");
-    printf("Вхідні дані: a = %.1f, b = %.1f, c = %.1f\n", a, b, c);
+    printf("Вхідні дані: a = %.1d, b = %.1d, c = %.1d\n", a, b, c);
 
-    // Перевірка на нуль 
-    if ((a + b + c) == 0 || (a * b * c) == 0) {
-        printf("\nПомилка: ділення на нуль! Перевірте вхідні дані.\n");
-    } else {
+    int term1 = (a * b * c) / (a + b + c);
+    int term2 = (a - b - c) / (a * b * c);
         
-        // Формула, яку я розбив на дві частини
-        double term1 = (a * b * c) / (a + b + c);
-        double term2 = (a - b - c) / (a * b * c);
-        
-        y = term1 - term2;
+    y = term1 - term2;
 
         // Результат
         printf("\nРезультат обчислення функції y = %.4f\n", y);
-    }
+    
 
     return 0;
 }
