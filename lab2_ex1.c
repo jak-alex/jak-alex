40cdf560f25403baf5c7f60fb7dabe66f0d1f540
jak-alex <alex.jakimik@gmail.com>
2026-03-16 18:24:32 +0200

    Refactor calculation logic in lab2_ex1.c

--- lab2_ex1.c
+++ lab2_ex1.c
@@ -5,7 +5,7 @@ int main() {
     // Оголошення змінних
     int num1, num2;
     int remainder, quotient, product;
-
+    
     printf("--- Програма для обчислення чисел (Варіант 6) ---\n");
 
     // Введення даних
@@ -15,24 +15,21 @@ int main() {
     printf("Введіть друге ціле число: ");
     scanf("%d", &num2);
 
-    // Перевірка у випадку, якщо одне зі значень = 0
-    if (num1 == 0 || num2 == 0) {
-        printf("\nERROR: ділення на нуль неможливе!\n");
-    } else {
-        remainder = num2 % num1;
+    remainder = num2 % num1;
 
-        quotient = num1 / num2;
+    quotient = num1 / num2;
 
-        product = num1 * num2;
+    product = num1 * num2;
 
-        // Результат
-        printf("\n--- Результати обчислень ---\n");
-        printf("1) Залишок від ділення %d на %d дорівнює: %d\n", num2, num1, remainder);
-        printf("2) Ціла частина від ділення %d на %d дорівнює: %d\n", num1, num2, quotient);
-        printf("3) Добуток чисел %d та %d дорівнює: %d\n", num1, num2, product);
-    }
+    printf("\n--- Результати обчислень ---\n");
+    printf("1) Залишок від ділення %d на %d дорівнює: %d\n", num2, num1, remainder);
+    printf("2) Ціла частина від ділення %d на %d дорівнює: %d\n", num1, num2, quotient);
+    printf("3) Добуток чисел %d та %d дорівнює: %d\n", num1, num2, product);
+    
+    
 
     printf("\nРоботу завершено.");
 
     return 0;
+    
 }
