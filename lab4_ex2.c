db26d8cb110843e86f87a4d31193932ee986fe6a
jak-alex <alex.jakimik@gmail.com>
2026-03-24 14:21:46 +0200

    Refactor average calculation program structure

--- lab4_ex2.c
+++ lab4_ex2.c
@@ -2,66 +2,75 @@
 
 int main() {
     int choice;
-    int sum, count, i;
-    float average;
+    
+    do {
+        int sum = 0, count = 0, i;
+        float average;
 
-    printf("Комплексна програма обчислення середнього арифметичного непарних [1;80]\n");
-    printf("Виберіть тип циклу для розрахунку:\n");
-    printf("1. А) Цикл з параметром (for)\n");
-    printf("2. Б) Цикл з передумовою (while)\n");
-    printf("3. В) Цикл з післяумовою (do...while)\n");
-    printf("Ваш вибір: ");
-    scanf("%d", &choice);
+        printf("\nМеню обчислення середнього арифметичного непарних [1;80]\n");
+        printf("1. Цикл з параметром (for)\n");
+        printf("2. Цикл з передумовою (while)\n");
+        printf("3. Цикл з післяумовою (do...while)\n");
+        printf("0. Вихід з програми\n");
+        printf("Ваш вибір: ");
+        
+        if (scanf("%d", &choice) != 1) {
+            printf("Помилка: введіть число!\n");
+            while(getchar() != '\n'); 
+            continue;
+        }
 
-    sum = 0;
-    count = 0;
+        if (choice == 0) {
+            printf("Завершення роботи. Бувай!\n");
+            break; 
+        }
 
-    printf("\nРезультат:\n");
-
-    switch (choice) {
-        case 1:
-            for (i = 1; i <= 80; i++) {
-                if (i % 2 != 0) {
-                    sum += i;
-                    count++;
+        switch (choice) {
+            case 1:
+                for (i = 1; i <= 80; i++) {
+                    if (i % 2 != 0) {
+                        sum += i;
+                        count++;
+                    }
                 }
-            }
-            printf("Використано цикл 'for'.\n");
-            break;
+                printf("\n[Результат]: Використано цикл 'for'.\n");
+                break;
 
-        case 2:
-            i = 1;
-            while (i <= 80) {
-                if (i % 2 != 0) {
-                    sum += i;
-                    count++;
+            case 2:
+                i = 1;
+                while (i <= 80) {
+                    if (i % 2 != 0) {
+                        sum += i;
+                        count++;
+                    }
+                    i++;
                 }
-                i++;
-            }
-            printf("Використано цикл 'while'.\n");
-            break;
+                printf("\n[Результат]: Використано цикл 'while'.\n");
+                break;
 
-        case 3:
-            i = 1;
-            do {
-                if (i % 2 != 0) {
-                    sum += i;
-                    count++;
-                }
-                i++;
-            } while (i <= 80);
-            printf("Використано цикл 'do...while'.\n");
-            break;
+            case 3:
+                i = 1;
+                do {
+                    if (i % 2 != 0) {
+                        sum += i;
+                        count++;
+                    }
+                    i++;
+                } while (i <= 80);
+                printf("\n[Результат]: Використано 'do...while'.\n");
+                break;
 
-        default:
-            printf("Помилка: Вибрано неіснуючий варіант!\n");
-            return 1;
-    }
+            default:
+                printf("\nПомилка: Вибрано неіснуючий варіант! Спробуйте ще раз.\n");
+                continue; 
+        }
 
-    if (count > 0) {
-        average = (float)sum / count;
-        printf("Середнє арифметичне непарних чисел: %.2f\n", average);
-    }
+        if (count > 0) {
+            average = (float)sum / count;
+            printf("Середнє арифметичне непарних чисел: %.2f\n", average);
+        }
+
+    } while (choice != 0); 
 
     return 0;
 }
