e393060cd65a8ef1389ea378756093b9d724c346
jak-alex <alex.jakimik@gmail.com>
2026-04-11 14:30:49 +0300

    Refactor matrix generation and add menu options

--- lab6_ex2.c
+++ lab6_ex2.c
@@ -3,60 +3,88 @@
 #include <time.h>
 
 int main() {
-    srand(time(NULL)); // Для випадкових чисел
+    srand(time(NULL)); // Випадкове число
+    int choice;
 
-    int n;
-    printf("Введіть розмір квадратної матриці (n): ");
-    scanf("%d", &n);
+    do {
+        // Меню
+        printf("\n========================================\n");
+        printf("    АНАЛІЗ ТА ТРАНСПОНУВАННЯ МАТРИЦІ    \n");
+        printf("========================================\n");
+        printf("1. Згенерувати матрицю та перевірити діагоналі\n");
+        printf("0. Вийти з програми\n");
+        printf("========================================\n");
+        printf("Ваш вибір: ");
+        scanf("%d", &choice);
 
-    double a[n][n];
-    double sum_main = 0, sum_side = 0;
+        if (choice == 1) {
+            int n;
 
-    // Генерируємо матрицю та виводимо її
-    printf("\nЗгенерована матриця:\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            
-            // Формула для double в діапазоні [-100, 100]
-            a[i][j] = ((double)rand() / RAND_MAX) * 200.0 - 100.0;
-            printf("%7.2f ", a[i][j]);
+            // Перевірка розміру (n має бути > 0)
+            do {
+                printf("\nВведіть розмір квадратної матриці (n > 0): ");
+                scanf("%d", &n);
+                if (n <= 0) {
+                    printf("Помилка! Розмір має бути додатним числом.\n");
+                }
+            } while (n <= 0);
 
-            // Рахуємо головну діагональ (i == j)
-            if (i == j) sum_main += a[i][j];
-            
-            // Рахуємо побічну діагональ (i + j == n - 1)
-            if (i + j == n - 1) sum_side += a[i][j];
-        }
-        printf("\n");
-    }
+            double a[n][n];
+            double sum_main = 0, sum_side = 0;
 
-    printf("\nСума головної діагоналі: %.2f", sum_main);
-    printf("\nСума побічної діагоналі: %.2f\n", sum_side);
+            // Генерація та вивід матриці
+            printf("\nЗгенерована матриця [%dx%d]:\n", n, n);
+            for (int i = 0; i < n; i++) {
+                printf("| ");
+                for (int j = 0; j < n; j++) {
+                    // Числа в діапазоні [-100.0, 100.0]
+                    a[i][j] = ((double)rand() / RAND_MAX) * 200.0 - 100.0;
+                    printf("%8.2f ", a[i][j]);
 
-    // Перевіряємоооо
-    if (sum_main > sum_side) {
-        printf("\nУмова виконана (головна > побічної). Транспонуємо матрицю...\n");
-        
-        // Транспонуємо
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                double temp = a[i][j];
-                a[i][j] = a[j][i];
-                a[j][i] = temp;
+                    // Сума головної діагоналі
+                    if (i == j) sum_main += a[i][j];
+                    
+                    // Сума побічної діагоналі
+                    if (i + j == n - 1) sum_side += a[i][j];
+                }
+                printf(" |\n");
             }
-        }
 
-        // Ось наш шедевр
-        printf("\nРезультат транспонування:\n");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                printf("%7.2f ", a[i][j]);
+            printf("\nСума головної діагоналі: %.2f\n", sum_main);
+            printf("Сума побічної діагоналі: %.2f\n", sum_side);
+
+            // Порівняння та транспонування
+            if (sum_main > sum_side) {
+                printf("\n[Умова виконана: %.2f > %.2f]\n", sum_main, sum_side);
+                printf("Транспонуємо матрицю (рядки стають стовпцями)...\n");
+                
+                for (int i = 0; i < n; i++) {
+                    for (int j = i + 1; j < n; j++) {
+                        double temp = a[i][j];
+                        a[i][j] = a[j][i];
+                        a[j][i] = temp;
+                    }
+                }
+
+                printf("\nРезультат транспонування:\n");
+                for (int i = 0; i < n; i++) {
+                    printf("| ");
+                    for (int j = 0; j < n; j++) {
+                        printf("%8.2f ", a[i][j]);
+                    }
+                    printf(" |\n");
+                }
+            } else {
+                printf("\n[Умова не виконана: %.2f <= %.2f]\n", sum_main, sum_side);
+                printf("Матриця залишається без змін.\n");
             }
-            printf("\n");
-        }
-    } else {
-        printf("\nУмова не виконана (головна <= побічної). Матриця залишається без змін.\n");
-    }
 
+        } else if (choice != 0) {
+            printf("\n[!] Невірна команда. Спробуйте ще раз.\n");
+        }
+
+    } while (choice != 0);
+
+    printf("\nПрограма завершена\n");
     return 0;
 }
